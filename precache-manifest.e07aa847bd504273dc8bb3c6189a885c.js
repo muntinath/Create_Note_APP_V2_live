2bc8cb1955ed17e58e940a00d0529f9b30b0bbe5
Munti Kumar Nath <muntinath@gmail.com>
2020-07-30 00:16:56 +0530

    Updates

---
+++ precache-manifest.e07aa847bd504273dc8bb3c6189a885c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4306bc4f554d906e7bb42d4271417539",
+    "url": "/Create_Note_APP_V2_live/index.html"
+  },
+  {
+    "revision": "332afed1439eb4df615b",
+    "url": "/Create_Note_APP_V2_live/static/css/main.ebb773b2.chunk.css"
+  },
+  {
+    "revision": "1cd29dfea82d34786e76",
+    "url": "/Create_Note_APP_V2_live/static/js/2.019562af.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Create_Note_APP_V2_live/static/js/2.019562af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "332afed1439eb4df615b",
+    "url": "/Create_Note_APP_V2_live/static/js/main.b26444ef.chunk.js"
+  },
+  {
+    "revision": "884a8cf80cb46be6775d",
+    "url": "/Create_Note_APP_V2_live/static/js/runtime-main.cc1dbe07.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Create_Note_APP_V2_live/precache-manifest.e07aa847bd504273dc8bb3c6189a885c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Create_Note_APP_V2_live/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
